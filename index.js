6c93a0fb12c14057c8d0a025c9e95dfc31c50bea
Naveen K M <naveenkmforums@gmail.com>
2018-09-06 05:27:54 +0530

    Intial code.

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const genres = require('./routes/genres');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use('/api/genres', genres);
+
+mongoose.connect('mongodb://localhost/vidly')
+.then(() => console.log('Connected to mongodb..'))
+.catch((err) => console.error('Could not connect to mongodb',err));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
