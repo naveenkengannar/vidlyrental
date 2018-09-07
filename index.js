f809f924929f067d722fee4f1cda75cc4fe4a9fd
Naveen K M <naveenkmforums@gmail.com>
2018-09-07 12:39:36 +0530

    Implemented Rentals api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const express = require('express');
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers',customers);
 app.use('/api/movies',movies);
+app.use('/api/rentals',rentals);
 
 mongoose.connect('mongodb://localhost/vidly',{ useNewUrlParser: true })
 .then(() => console.log('Connected to mongodb..'))
